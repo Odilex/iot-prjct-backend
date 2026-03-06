d1df7b4510f2df46ae7ee39c4a516075c42be8c5
odilex <didierjunior75@gmail.com>
2026-03-06 14:33:24 +0200

    19

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,8 +4,8 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
-    directUrl: env("DIRECT_URL"),
+    url: env("DATABASE_URL") || "",
+    directUrl: env("DIRECT_URL") || "",
     schemas: ["public", "auth"],
   },
 });
